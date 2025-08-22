40e9442116f888987a024a70c5300fe037ec38a7
Daryna <kisildarina09@gmail.com>
2025-08-22 16:32:29 +0300

    attempt to write in another way

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,6 @@ import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = document.getElementById('root')
+
+ReactDom.createRoot(root).render(<App />)
