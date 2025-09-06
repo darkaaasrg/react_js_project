f0a78fba90a2b8f523b7d4edacdc8f0215fba813
F1xik404 <illianovosad28@gmail.com>
2025-09-06 20:13:55 +0300

    Merge conflict reaolving

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ export default function App(){
     <div>
       <Header />
      <main>
-      <h1>Hello React!!</h1>
+         <h1>DARKA THE BEST OF THE BEST EVER FROM ALL THE PEOPLE I KNOW!</h1>
       </main>
     </div>
   )
 }
-
-
